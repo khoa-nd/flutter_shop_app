c6dda1a418624f300fbdf7e73cb950f98522bb12
Khoa Nguyen <khoa.nguyen@adaptis.io>
2023-02-02 16:47:50 +0700

    query product by id based on provider in the detail screen

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -45,4 +45,8 @@ class Products with ChangeNotifier {
     // _items.add(value);
     notifyListeners();
   }
+
+  Product findById(String id) {
+    return _items.firstWhere((product) => product.id == id);
+  }
 }
