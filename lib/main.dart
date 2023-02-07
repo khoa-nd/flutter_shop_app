6ef79988d548c49773289266fb6eb0a286156a3f
Khoa Nguyen <khoa.nguyen@adaptis.io>
2023-02-07 20:58:36 +0700

    adding manage products page

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_shop_app/providers/orders.dart';
 import 'package:flutter_shop_app/providers/products.dart';
 import 'package:flutter_shop_app/screens/cart_screen.dart';
 import 'package:flutter_shop_app/screens/orders_screen.dart';
+import 'package:flutter_shop_app/screens/user_products_screen.dart';
 import 'package:provider/provider.dart';
 
 import './screens/products_overview_screen.dart';
@@ -31,7 +32,8 @@ class MyApp extends StatelessWidget {
             routes: {
               ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
               CartScreen.routeName: (ctx) => CartScreen(),
-              OrderScreen.routeName: (ctx) => OrderScreen()
+              OrderScreen.routeName: (ctx) => OrderScreen(),
+              UserProductsScreen.routeName: (ctx) => UserProductsScreen()
             }));
   }
 }
