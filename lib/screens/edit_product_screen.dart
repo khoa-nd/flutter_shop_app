5c3723a1b16046371f1cfe9301ba87504bcb1985
Khoa Nguyen <khoa.nguyen@adaptis.io>
2023-02-07 21:30:41 +0700

    manage focus input

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = '/edit-product';
+
   @override
   State<EditProductScreen> createState() => _EditProductScreenState();
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,12 +20,20 @@ class _EditProductScreenState extends State<EditProductScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Form(
             child: ListView(
-          children: <Widget>[
-            TextFormField(
-                decoration: InputDecoration(labelText: 'Title'),
-                textInputAction: TextInputAction.next)
-          ],
-        )),
+              children: <Widget>[
+                TextFormField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    textInputAction: TextInputAction.next,
+                    onFieldSubmitted: (_) {
+                      FocusScope.of(context).requestFocus(_priceFocusNode);
+                    }),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Price'),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  focusNode: _priceFocusNode,)
+              ],
+            )),
       ),
     );
   }
