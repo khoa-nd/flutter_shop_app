972643670e7dfd26543e4d3b977b711ab63ed3bb
Khoa Nguyen <khoa.nguyen@adaptis.io>
2023-02-07 20:28:09 +0700

    using snackbar and impl UNDO cart item

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -6,11 +6,10 @@ class CartItem {
   final int quantity;
   final double price;
 
-  CartItem(
-      {@required this.id,
-      @required this.title,
-      @required this.quantity,
-      @required this.price});
+  CartItem({@required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.price});
 }
 
 class Cart with ChangeNotifier {
@@ -36,19 +35,21 @@ class Cart with ChangeNotifier {
     if (_items.containsKey(productId)) {
       _items.update(
           productId,
-          (existingCartItem) => CartItem(
-              id: existingCartItem.id,
-              title: existingCartItem.title,
-              quantity: existingCartItem.quantity + 1,
-              price: existingCartItem.price));
+              (existingCartItem) =>
+              CartItem(
+                  id: existingCartItem.id,
+                  title: existingCartItem.title,
+                  quantity: existingCartItem.quantity + 1,
+                  price: existingCartItem.price));
     } else {
       _items.putIfAbsent(
           productId,
-          () => CartItem(
-              id: productId,
-              title: title,
-              quantity: 1,
-              price: price));
+              () =>
+              CartItem(
+                  id: productId,
+                  title: title,
+                  quantity: 1,
+                  price: price));
     }
 
     notifyListeners();
@@ -61,6 +62,22 @@ class Cart with ChangeNotifier {
 
   void clear() {
     _items = {};
+    notifyListeners();
+  }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId].quantity > 1) {
+      _items.update(productId, (existingCartItem) =>
+          CartItem(id: existingCartItem.id,
+              title: existingCartItem.title,
+              quantity: existingCartItem.quantity - 1,
+              price: existingCartItem.price));
+    } else {
+      _items.remove(productId);
+    }
     notifyListeners();
   }
 }
